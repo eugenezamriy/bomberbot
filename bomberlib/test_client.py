493facda2f3dcb43eb1748c75e2f82fb15d7134d
longedok <longedok@gmail.com>
2010-06-23 18:52:24 +0300

    Added an small test client.

---
+++ bomberlib/test_client.py
@@ -0,0 +1,27 @@
+# encoding: utf8
+import socket, sys, struct
+
+def send(socket, message):
+    """ Sends a message with null-terminator."""
+    totalsent = 0
+    while totalsent < len(message):
+        sent = socket.send(message[totalsent:])
+        if sent == 0:
+            raise RuntimeError, "Socket connection broken"
+        totalsent += sent
+    socket.send("\x00")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print "Usage: %s host port message" % sys.argv[0]
+    else:
+        host, port, message = sys.argv[1:]
+        port = int(port)
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((host, port))
+            print "Request: ", message
+            send(sock, message)
+            sock.close()
+        except Exception, err:
+            print "Error: %s\n" % str(err)
