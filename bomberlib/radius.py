27f2755ec785b25c29c0c78b820795baae469e58
longedok <longedok@gmail.com>
2010-07-08 13:22:49 +0300

    added radius.py

---
+++ bomberlib/radius.py
@@ -0,0 +1,16 @@
+# -*- mode:python; coding:utf-8; -*-
+# created: 06.07.2010 19:19
+# description: Bomberbot radius bonuse representation
+
+
+class Radius(object):
+
+    def __init__(self, x, y):
+        """
+        @type x:  int
+        @param x: X position.
+        @type y:  int
+        @param y: Y position.
+        """
+        self.x = x
+        self.y = y
